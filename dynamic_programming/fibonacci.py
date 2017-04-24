86ae88a39a5d80c06ad3a596cbd12b4d9b0d5906
Nasreen123 <nabuhunaina@protonmail.com>
2017-04-23 21:40:22 -0400

    done with @Dzol

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -3,3 +3,24 @@
 dictionary = {0: 0, 1: 1}
 
 def fibonacci(n):
+    if n in dictionary:
+        return dictionary[n]
+    else:
+        dictionary[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return dictionary[n]
+
+def test():
+
+    assert 0 == fibonacci(0)
+    assert 1 == fibonacci(1)
+
+    assert 1 == fibonacci(2)
+    assert 2 == fibonacci(3)
+    assert 3 == fibonacci(4)
+    assert 5 == fibonacci(5)
+    assert 8 == fibonacci(6)
+
+    print "success"
+
+if __name__ == "__main__":
+    test()
